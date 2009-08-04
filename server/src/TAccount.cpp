d577131bdd99917dd0f814d438a37cd9a84f9d81
loneboco <loneboco@7aab1f78-3e5b-11de-917f-4137a9cc17b9>
2009-08-04 20:51:10 +0000

    Fixed 1.41 support.  Files send correctly now, player and NPC props use the correct images, baddies display correctly, and signs no longer display #K(13). Check if a .graal or .zelda file is actually a level before loading it. Fixed a problem where language could be empty.

--- server/src/TAccount.cpp
+++ server/src/TAccount.cpp
@@ -131,7 +131,7 @@ bool TAccount::loadAccount(const CString& pAccount)
 		else if (section == "APCOUNTER") apCounter = strtoint(val);
 		else if (section == "ONSECS") onlineTime = strtoint(val);
 		else if (section == "IP") accountIp = strtoint(val);
-		else if (section == "LANGUAGE") language = val;
+		else if (section == "LANGUAGE") { language = val; if (language.isEmpty()) language = "English"; }
 		else if (section == "KILLS") kills = strtoint(val);
 		else if (section == "DEATHS") deaths = strtoint(val);
 		else if (section == "RATING") rating = (float)strtofloat(val);
