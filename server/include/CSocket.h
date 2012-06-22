070ec093d91fef43f69665efd54b3267871ef4e7
loneboco <loneboco@7aab1f78-3e5b-11de-917f-4137a9cc17b9>
2012-06-22 15:16:29 +0000

    Fixing a mingw issue with CSocket.

--- server/include/CSocket.h
+++ server/include/CSocket.h
@@ -36,9 +36,9 @@
 		#define WIN32_LEAN_AND_MEAN
 	#endif
 
-	//#ifndef WINVER
-	//	#define WINVER 0x0501
-	//#endif
+	#ifndef _WIN32_WINNT
+		#define _WIN32_WINNT 0x0501
+	#endif
 	#include <winsock2.h>
 	#include <ws2tcpip.h>
 	#include <windows.h>
