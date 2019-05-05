2111d7b8d7d30a888bbc7b0f9ab214ac185abd6e
joey <1166538+xtjoeytx@users.noreply.github.com>
2019-05-05 11:21:20 -0400

    Adding a note to ~IScriptWrapped regarding an assert that may get triggered.

--- server/include/script/ScriptWrapped.h
+++ server/include/script/ScriptWrapped.h
@@ -21,6 +21,10 @@ public:
 	}
 	
 	virtual ~IScriptWrapped() {
+		// TODO(joey): This assert is triggered when updating levels quickly. The reason for this
+		// is because npcs may have actions queued up, and referenceCount doesn't decrease on destructor
+		// only when the action is invoked and the arguments are parsed. Will look into this, but shouldn't
+		// have any side effects anyway.
 		assert(_referenceCount == 0);
 	}
 	
