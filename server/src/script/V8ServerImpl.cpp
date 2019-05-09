a2267387f7e34c28c271c7996b09fd88674c65ad
joey <1166538+xtjoeytx@users.noreply.github.com>
2019-05-09 10:22:51 -0400

    Changes to npc-server: - Can iterate through server.npcs - *may not stay*

--- server/src/script/V8ServerImpl.cpp
+++ server/src/script/V8ServerImpl.cpp
@@ -12,6 +12,7 @@
 
 // TODO(joey): Currently not cleaning this up
 v8::Persistent<v8::FunctionTemplate> _persist_server_flags_ctor;
+v8::Persistent<v8::FunctionTemplate> _persist_server_npcs_ctor;
 
 void Server_SendToNC(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
@@ -55,10 +56,10 @@ void Server_GetObject_Flags(v8::Local<v8::String> prop, const v8::PropertyCallba
     v8::Local<v8::Object> self = info.This();
     TServer *serverObject = UnwrapObject<TServer>(self);
 
-    v8::Local<v8::String> internalFlags = v8::String::NewFromUtf8(isolate, "_internalFlags", v8::NewStringType::kInternalized).ToLocalChecked();
-    if (self->HasRealNamedProperty(context, internalFlags).ToChecked())
+    v8::Local<v8::String> internalProperty = v8::String::NewFromUtf8(isolate, "_internalFlags", v8::NewStringType::kInternalized).ToLocalChecked();
+    if (self->HasRealNamedProperty(context, internalProperty).ToChecked())
     {
-        info.GetReturnValue().Set(self->Get(context, internalFlags).ToLocalChecked());
+        info.GetReturnValue().Set(self->Get(context, internalProperty).ToLocalChecked());
         return;
     }
 
@@ -67,7 +68,7 @@ void Server_GetObject_Flags(v8::Local<v8::String> prop, const v8::PropertyCallba
     new_instance->SetAlignedPointerInInternalField(0, serverObject);
 
     v8::PropertyAttribute propAttr = static_cast<v8::PropertyAttribute>(v8::PropertyAttribute::ReadOnly | v8::PropertyAttribute::DontDelete | v8::PropertyAttribute::DontEnum);
-    self->DefineOwnProperty(context, internalFlags, new_instance, propAttr).FromJust();
+    self->DefineOwnProperty(context, internalProperty, new_instance, propAttr).FromJust();
     info.GetReturnValue().Set(new_instance);
 }
 
@@ -121,6 +122,51 @@ void Server_Flags_Enumerator(const v8::PropertyCallbackInfo<v8::Array>& info)
 	info.GetReturnValue().Set(result);
 }
 
+// PROPERTY: Server Npcs
+void Server_GetObject_Npcs(v8::Local<v8::String> prop, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+	v8::Isolate *isolate = info.GetIsolate();
+	v8::Local<v8::Context> context = isolate->GetCurrentContext();
+	v8::Local<v8::Object> self = info.This();
+	TServer *serverObject = UnwrapObject<TServer>(self);
+
+	v8::Local<v8::String> internalProperty = v8::String::NewFromUtf8(isolate, "_internalNpcs", v8::NewStringType::kInternalized).ToLocalChecked();
+	if (self->HasRealNamedProperty(context, internalProperty).ToChecked())
+	{
+		info.GetReturnValue().Set(self->Get(context, internalProperty).ToLocalChecked());
+		return;
+	}
+
+	v8::Local<v8::FunctionTemplate> ctor_tpl = PersistentToLocal(isolate, _persist_server_npcs_ctor);
+	v8::Local<v8::Object> new_instance = ctor_tpl->InstanceTemplate()->NewInstance(context).ToLocalChecked();
+	new_instance->SetAlignedPointerInInternalField(0, serverObject);
+
+	v8::PropertyAttribute propAttr = static_cast<v8::PropertyAttribute>(v8::PropertyAttribute::ReadOnly | v8::PropertyAttribute::DontDelete | v8::PropertyAttribute::DontEnum);
+	self->DefineOwnProperty(context, internalProperty, new_instance, propAttr).FromJust();
+	info.GetReturnValue().Set(new_instance);
+}
+
+void Server_Npcs_Enumerator(const v8::PropertyCallbackInfo<v8::Array>& info)
+{
+	v8::Isolate *isolate = info.GetIsolate();
+	v8::Local<v8::Context> context = isolate->GetCurrentContext();
+	v8::Local<v8::Object> self = info.This();
+	TServer *serverObject = UnwrapObject<TServer>(self);
+
+	// Get npcs list
+	auto npcList = serverObject->getNPCList();
+
+	v8::Local<v8::Array> result = v8::Array::New(isolate, npcList->size());
+
+	int idx = 0;
+	for (auto it = npcList->begin(); it != npcList->end(); ++it) {
+		V8ScriptWrapped<TNPC> *v8_wrapped = static_cast<V8ScriptWrapped<TNPC> *>((*it)->getScriptObject());
+		result->Set(context, idx++, v8_wrapped->Handle(isolate)).Check();
+	}
+
+	info.GetReturnValue().Set(result);
+}
+
 void bindClass_Server(CScriptEngine *scriptEngine)
 {
 	// Retrieve v8 environment
@@ -146,6 +192,7 @@ void bindClass_Server(CScriptEngine *scriptEngine)
 
 	// Properties
 	server_proto->SetAccessor(v8::String::NewFromUtf8(isolate, "flags"), Server_GetObject_Flags);
+	server_proto->SetAccessor(v8::String::NewFromUtf8(isolate, "npcs"), Server_GetObject_Npcs);
 
     // Create the server flags template
 	v8::Local<v8::FunctionTemplate> server_flags_ctor = v8::FunctionTemplate::New(isolate);
@@ -155,6 +202,15 @@ void bindClass_Server(CScriptEngine *scriptEngine)
 			Server_Flags_Getter, Server_Flags_Setter, nullptr, nullptr, Server_Flags_Enumerator, v8::Local<v8::Value>(),
 			v8::PropertyHandlerFlags::kOnlyInterceptStrings));
 	_persist_server_flags_ctor.Reset(isolate, server_flags_ctor);
+
+	// Create the server npcs template
+	v8::Local<v8::FunctionTemplate> server_npcs_ctor = v8::FunctionTemplate::New(isolate);
+	server_npcs_ctor->SetClassName(v8::String::NewFromUtf8(isolate, "npcs"));
+	server_npcs_ctor->InstanceTemplate()->SetInternalFieldCount(1);
+	server_npcs_ctor->InstanceTemplate()->SetHandler(v8::NamedPropertyHandlerConfiguration(
+			nullptr, nullptr, nullptr, nullptr, Server_Npcs_Enumerator, v8::Local<v8::Value>(),
+			v8::PropertyHandlerFlags::kOnlyInterceptStrings));
+	_persist_server_npcs_ctor.Reset(isolate, server_npcs_ctor);
 
 	// Properties...?
 	//server_proto->SetAccessor(v8::String::NewFromUtf8(isolate, "id"), NPC_GetInt32_npc_id, NPC_SetInt32_npc_id);
