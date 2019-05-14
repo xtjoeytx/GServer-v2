9e89f172fae730aea365ee4133ed9c494b423604
死体 <xx.shitai.xx@gmail.com>
2019-05-14 13:02:10 +0200

    Spaces to tabs Remove .gclient files as they cause v8 not to be built

--- cmake/FindV8.cmake
+++ cmake/FindV8.cmake
@@ -7,85 +7,85 @@
 # V8_INCLUDE_DIR, where to find the headers
 
 IF (NOT $ENV{V8_DIR} STREQUAL "")
-  SET(V8_DIR $ENV{V8_DIR})
+	SET(V8_DIR $ENV{V8_DIR})
 ENDIF()
 
 
 SET(V8_LIBRARY_SEARCH_PATHS
-  ${V8_DIR}/
-  ${V8_DIR}/lib/
-  ${V8_DIR}/build/Release/lib/
-  ${V8_DIR}/build/Release/lib/third_party/icu/
-  ${V8_DIR}/build/Release/obj/
-  ${V8_DIR}/build/Release/obj/third_party/icu/
-  ${V8_DIR}/out/ia32.release/lib.target/
-  ${V8_DIR}/out/ia32.release/lib.target/third_party/icu/
-  ${V8_DIR}/out/ia32.release/obj/
-  ${V8_DIR}/out/ia32.release/obj/third_party/icu/
-  ${V8_DIR}/out.gn/ia32.release/lib.target/
-  ${V8_DIR}/out.gn/ia32.release/lib.target/third_party/icu/
-  ${V8_DIR}/out.gn/ia32.release/obj/
-  ${V8_DIR}/out.gn/ia32.release/obj/third_party/icu/
-  ${V8_DIR}/out/x64.release/lib.target/
-  ${V8_DIR}/out/x64.release/lib.target/third_party/icu/
-  ${V8_DIR}/out/x64.release/obj/
-  ${V8_DIR}/out/x64.release/obj/third_party/icu/
-  ${V8_DIR}/out.gn/x64.release/lib.target/
-  ${V8_DIR}/out.gn/x64.release/lib.target/third_party/icu/
-  ${V8_DIR}/out.gn/x64.release/obj/
-  ${V8_DIR}/out.gn/x64.release/obj/third_party/icu/
-  ${V8_DIR}/out.gn/x64.release.sample/lib.target/
-  ${V8_DIR}/out.gn/x64.release.sample/lib.target/third_party/icu/
-  ${V8_DIR}/out.gn/x64.release.sample/obj/
-  ${V8_DIR}/out.gn/x64.release.sample/obj/third_party/icu/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/lib.target/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/lib.target/third_party/icu/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/obj/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/obj/third_party/icu/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/lib.target/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/lib.target/third_party/icu/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/obj/
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/obj/third_party/icu/
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local/lib
-  /usr/lib
-  /sw/lib
-  /opt/local/lib
-  /opt/csw/lib
-  /opt/lib
-  /usr/freeware/lib64
+	${V8_DIR}/
+	${V8_DIR}/lib/
+	${V8_DIR}/build/Release/lib/
+	${V8_DIR}/build/Release/lib/third_party/icu/
+	${V8_DIR}/build/Release/obj/
+	${V8_DIR}/build/Release/obj/third_party/icu/
+	${V8_DIR}/out/ia32.release/lib.target/
+	${V8_DIR}/out/ia32.release/lib.target/third_party/icu/
+	${V8_DIR}/out/ia32.release/obj/
+	${V8_DIR}/out/ia32.release/obj/third_party/icu/
+	${V8_DIR}/out.gn/ia32.release/lib.target/
+	${V8_DIR}/out.gn/ia32.release/lib.target/third_party/icu/
+	${V8_DIR}/out.gn/ia32.release/obj/
+	${V8_DIR}/out.gn/ia32.release/obj/third_party/icu/
+	${V8_DIR}/out/x64.release/lib.target/
+	${V8_DIR}/out/x64.release/lib.target/third_party/icu/
+	${V8_DIR}/out/x64.release/obj/
+	${V8_DIR}/out/x64.release/obj/third_party/icu/
+	${V8_DIR}/out.gn/x64.release/lib.target/
+	${V8_DIR}/out.gn/x64.release/lib.target/third_party/icu/
+	${V8_DIR}/out.gn/x64.release/obj/
+	${V8_DIR}/out.gn/x64.release/obj/third_party/icu/
+	${V8_DIR}/out.gn/x64.release.sample/lib.target/
+	${V8_DIR}/out.gn/x64.release.sample/lib.target/third_party/icu/
+	${V8_DIR}/out.gn/x64.release.sample/obj/
+	${V8_DIR}/out.gn/x64.release.sample/obj/third_party/icu/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/lib.target/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/lib.target/third_party/icu/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/obj/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release/obj/third_party/icu/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/lib.target/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/lib.target/third_party/icu/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/obj/
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/out.gn/x64.release.sample/obj/third_party/icu/
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/lib
+	/usr/lib
+	/sw/lib
+	/opt/local/lib
+	/opt/csw/lib
+	/opt/lib
+	/usr/freeware/lib64
 )
 
 FIND_PATH(V8_INCLUDE_DIR v8.h
-  ${V8_DIR}
-  ${V8_DIR}/include
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8
-  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/include
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local/include
-  /usr/include
-  /sw/include # Fink
-  /opt/local/include # DarwinPorts
-  /opt/csw/include # Blastwave
-  /opt/include
-  /usr/freeware/include
-  /devel
+	${V8_DIR}
+	${V8_DIR}/include
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8
+	${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/v8/include
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/include
+	/usr/include
+	/sw/include # Fink
+	/opt/local/include # DarwinPorts
+	/opt/csw/include # Blastwave
+	/opt/include
+	/usr/freeware/include
+	/devel
 )
 
 FIND_LIBRARY(V8_LIBRARY
-  NAMES libv8_monolith.a v8_monolith.a v8_monolith libv8_monolith
-  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+	NAMES libv8_monolith.a v8_monolith.a v8_monolith libv8_monolith
+	PATHS ${V8_LIBRARY_SEARCH_PATHS}
 )
 
 SET(V8_FOUND "NO")
 
 IF (NOT UNIX)
-  IF (V8_LIBRARY AND V8_INCLUDE_DIR)
-    SET(V8_FOUND "YES")
-  ENDIF()
+	IF (V8_LIBRARY AND V8_INCLUDE_DIR)
+		SET(V8_FOUND "YES")
+	ENDIF()
 ELSEIF(V8_LIBRARY AND V8_INCLUDE_DIR)
-  SET(V8_FOUND "YES")
+	SET(V8_FOUND "YES")
 ENDIF(NOT UNIX)
 
