4ba03fda9c9615abfa3def76f31ab85da5e13e7d
死体 <xx.shitai.xx@gmail.com>
2020-10-12 12:39:14 +0200

    Reload weapons if a new .dump file is uploaded or anything in weapons folder

--- server/src/TPlayerRC.cpp
+++ server/src/TPlayerRC.cpp
@@ -2126,7 +2126,7 @@ void updateFile(TPlayer* player, TServer* server, CString& dir, CString& file)
 	}
 
 	// If folder config is off, add it to the file list.
-	if (settings->getBool("nofoldersconfig", false) == true)
+	if ( settings->getBool("nofoldersconfig", false))
 	{
 		CFileSystem* fs = server->getFileSystem();
 		if (fs->find(file).isEmpty())
@@ -2136,11 +2136,11 @@ void updateFile(TPlayer* player, TServer* server, CString& dir, CString& file)
 	else
 	{
 		std::vector<CString> foldersConfig = CString::loadToken(CString() << server->getServerPath() << "config/foldersconfig.txt", "\n", true);
-		for (std::vector<CString>::iterator i = foldersConfig.begin(); i != foldersConfig.end(); ++i)
+		for (auto & folderConfig : foldersConfig)
 		{
-			CString type = i->readString(" ").trim();
+			CString type = folderConfig.readString(" ").trim();
 			CString folder("world/");
-			folder << i->readString("").trim();
+			folder << folderConfig.readString("").trim();
 
 			if (fullPath.match(folder))
 			{
@@ -2168,6 +2168,8 @@ void updateFile(TPlayer* player, TServer* server, CString& dir, CString& file)
 		TLevel* l = TLevel::findLevel(file, server);
 		if (l) l->reload();
 	}
+	else if (ext == ".dump" || dir.findi(CString("weapons")) > -1)
+		server->loadWeapons(true);
 	else if (file == "serveroptions.txt")
 	{
 		server->loadSettings();
