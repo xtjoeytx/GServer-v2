c948030e2c5b80b3013b588810de132c1586fff9
死体 <xx.shitai.xx@gmail.com>
2022-11-12 19:25:43 +0100

    Fix setting tiles from scripts

--- server/src/Scripting/v8/V8LevelImpl.cpp
+++ server/src/Scripting/v8/V8LevelImpl.cpp
@@ -159,10 +159,9 @@ void Level_Tile_Setter(uint32_t index, v8::Local<v8::Value> value, const v8::Pro
 	{
 		// Get new value
 		unsigned int newValue = value->Uint32Value(info.GetIsolate()->GetCurrentContext()).ToChecked();
-		if (newValue > 32) // Unsure how many colors exist, capping at 32 for now
-			newValue = 32;
 
-		levelObject->getTiles()[index] = (short)newValue;
+		auto pTileData = CString() >> (short)newValue;
+		levelObject->alterBoard(pTileData, index % 64, index / 64, 1, 1, nullptr);
 	}
 
 	// Needed to indicate we handled the request
