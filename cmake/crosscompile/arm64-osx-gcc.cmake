1f7d6aed9399b17a7f1e0d29b34b2ebb11a283ba
John Norman <loneboco@gmail.com>
2026-02-07 19:02:02 -0800

    Disable some compiler flags that stop GCC 15 builds on OSX.

--- cmake/crosscompile/arm64-osx-gcc.cmake
+++ cmake/crosscompile/arm64-osx-gcc.cmake
@@ -5,4 +5,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 
+set(VCPKG_CXX_FLAGS_RELEASE -Wno-error=type-limits -Wno-type-limits -Wno-error=stringop-overflow -Wno-stringop-overflow)
+set(VCPKG_C_FLAGS_RELEASE -Wno-error=type-limits -Wno-type-limits -Wno-error=stringop-overflow -Wno-stringop-overflow)
+
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/osx-gcc-15-toolchain.cmake")
