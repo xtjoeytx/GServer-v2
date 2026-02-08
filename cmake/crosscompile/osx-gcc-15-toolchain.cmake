ae34a40c8c46a2a95194f1273cefb8d831411daf
John Norman <loneboco@gmail.com>
2026-02-07 17:18:25 -0800

    Adding a custom toolchain.

---
+++ cmake/crosscompile/osx-gcc-15-toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER "/opt/homebrew/bin/gcc-15")
+set(CMAKE_CXX_COMPILER "/opt/homebrew/bin/g++-15")
+
+set(CMAKE_OSX_SYSROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX26.2.sdk)
